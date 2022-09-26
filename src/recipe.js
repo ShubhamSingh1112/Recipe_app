9ee1ad0d097379d58f4e50fb68ce4c7ce8b4e2a1
Shubham Singh <46527607+ShubhamSingh1112@users.noreply.github.com>
2022-09-26 23:12:58 +0530

    Design Changes

--- src/recipe.js
+++ src/recipe.js
@@ -4,14 +4,14 @@ import style from './recipe.module.css';
 const Recipe = ({ title, calories, ingredients, image }) => {
     return(
         <div className={style.recipe}>
+            <img className={style.image} src={image} alt="" />
              <h1>{title}</h1>
              <ol>
                  {ingredients.map(ingredient => (
                      <li>{ingredient.text}</li>
                  ))}
              </ol>
-             <p>{calories}</p>
-             <img className={style.image} src={image} alt="" />
+             <p>Calories: {calories}</p>
         </div>
         );
 } 
