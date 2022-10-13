fdede24e534ec08dbe74838643fc51be2050709c
SHubhamSingh1112 <singhshubhamsingh2208@outlook.com>
2022-10-13 23:06:00 +0530

    api search error fix

--- src/App.js
+++ src/App.js
@@ -1,61 +1,59 @@
-import React, { useEffect, useState } from "react"; 
-import Recipe from './recipe';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Recipe from './recipe';
 
 const App = () => {
   const APP_ID = `5d292ee0`;
   const APP_KEY = `0d3e747bee69ea48b3901e1d4f8a8c6f`;
-
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState('chicken');
-  
-
-  useEffect(() => {
-    getRecipes();
-  }, [query]);
-
+const [recipes, setRecipes] = useState([]);
+const [search, setSearch] = useState("");
+const [query, setQuery] = useState("chicken");
+useEffect(() => {
   const getRecipes = async () => {
-    const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-    );
-  const data = await response.json();
-  setRecipes(data.hits);
+    const response = await fetch
+      (`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+    // console.log(data);
+  
   };
+	getRecipes();
+}, [APP_ID, APP_KEY, query])
 
-  const updateSearch = e => {
-    setSearch(e.target.value);
-  };
-
-  const getSearch = e => {
-    e.preventDefault();
-    setQuery(search);
-    setSearch('');
-  };
-
-  return (
-    <div className="App">
-      <h1 align="center">Recipe App</h1>
-      <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
-        <button className="search-button" type="submit">
-          Search
-        </button>
-      </form>
-      <div className="recipes">
-      {recipes.map(recipe => (
-        <Recipe 
-        key={recipe.recipe.label}
-        title={recipe.recipe.label} 
-        claories={recipe.recipe.calories}
-        image={recipe.recipe.image}
-        ingredients={recipe.recipe.ingredients}
-        />
-      ))}
-      </div>
-      <footer align="center">&copy; Shubham Singh 2021</footer>
-    </div>
-  );
+const updateSearch = e => {
+	setSearch(e.target.value);
 };
+const getSearch = e => {
+	e.preventDefault();
+	setQuery(search);
+	setSearch("");
+}
+
+return (
+	<div className="App">
+	<form className="search-form" onSubmit={getSearch} >
+		<input className="search-bar" type="text" value={search}
+			onChange={updateSearch} />
+		<button className="search-button" type="submit" >
+			Search
+		</button>
+	</form>
+	<div className="recipes">
+		{recipes.map(recipe => (
+		<Recipe
+			key={recipe.recipe.label}
+			title={recipe.recipe.label}
+			calories={recipe.recipe.calories}
+			image={recipe.recipe.image}
+			ingredients={recipe.recipe.ingredients}
+		/>
+
+		))}
+	</div>
+  <footer align="center">&copy; Shubham Singh 2022</footer>
+	</div>
+);
+}
 
 export default App;
+
